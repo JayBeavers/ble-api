2ce096b8102aa0aac8fda5179853572d95db0705
rgrover1 <devnull@localhost>
2014-11-21 09:23:23 +0000

    Synchronized with git rev ae371eaf Author: Rohit Grover add a missing header include.

--- public/GattAttribute.h
+++ public/GattAttribute.h
@@ -18,6 +18,8 @@
 #ifndef __GATT_ATTRIBUTE_H__
 #define __GATT_ATTRIBUTE_H__
 
+#include <stddef.h>
+
 #include "blecommon.h"
 #include "UUID.h"
 
@@ -93,4 +95,4 @@ protected:
     Handle_t  _handle;
 };
 
-#endif // ifndef __GATT_ATTRIBUTE_H__
+#endif // ifndef __GATT_ATTRIBUTE_H__
